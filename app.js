869545414fa9a4b30cc67ae4dc0deb4f173546e3
nareshs-hexaware <112936878+nareshs-hexaware@users.noreply.github.com>
2022-11-17 08:41:53 +0530

    added server close

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
-const express=require("express")
-const app=express()
+const express = require("express")
+const app = express()
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
     res.status(201).send("get call")
 })
 
-module.exports=app
+module.exports = app
 
-app.listen(3000,()=>{
-    console.log("Server is running on 3000")
+var server = app.listen(4000, () => {
+    console.log("Server is running on 4000")
 })
+
+server.close()
